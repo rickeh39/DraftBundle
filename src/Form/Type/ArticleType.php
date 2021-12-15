59ed5f78e5b3dd096d4a4a199b2c6030e6ef7d94
marloesbijkerk <marloesbijkerk@gmail.com>
2021-12-15 16:29:14 +0100

    styling article page

--- src/Form/Type/ArticleType.php
+++ src/Form/Type/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 use App\Document\Article;
 use Doctrine\ODM\MongoDB\Types\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,9 +15,18 @@ class ArticleType extends \Symfony\Component\Form\AbstractType {
         $builder
             ->add('title', TextType::class)
             ->add('description', TextType::class)
-            ->add('content', TextType::class)
-            ->add('save', SubmitType::class)
-        ;
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'tinymce',
+                    'rows' => 10,
+                    'style' => 'width: 100%'
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-lg btn-success btn-block'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
